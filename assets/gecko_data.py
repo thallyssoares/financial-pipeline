639af8a78ab356989b05e71e8d1cae453f29b68f
Thallys <antoniothallys2017@gmail.com>
2026-06-24 16:15:38 -0300

    feature: logging implementation inside resources and gecko_data asset

--- assets/gecko_data.py
+++ assets/gecko_data.py
@@ -3,7 +3,9 @@ from datetime import datetime
 import pandas as pd
 from dagster import asset
 
-from resources import BqResource, CoingeckoResource
+from resources import BqResource, CoingeckoResource, get_dagster_logger
+
+logger = get_dagster_logger()
 
 
 @asset()
@@ -13,18 +15,30 @@ def price_df(gecko: CoingeckoResource, bq: BqResource) -> None:
     coins = ["bitcoin", "solana", "ethereum"]
 
     rows = []
+    logger.info(f"Iniciando a extração de dados do Coingecko para {len(coins)} moedas")
 
     for c in coins:
-        response = client.coins.get_id(c)
+        logger.info(f"Buscando dados para a moeda: {c}")
+        try:
+            response = client.coins.get_id(c)
 
-        json_payload = response.model_dump_json(indent=2)
+            json_payload = response.model_dump_json(indent=2)
 
-        rows.append(
-            {
-                "ingestion_timestamp": datetime.now().isoformat(),
-                "raw_payload": json_payload,
-            }
-        )
-
+            rows.append(
+                {
+                    "ingestion_timestamp": datetime.now().isoformat(),
+                    "raw_payload": json_payload,
+                }
+            )
+            logger.info(f"Dados da moeda {c} extraidos com sucesso")
+        except Exception as e:
+            logger.error(
+                f"Não foi possivel extrair os dados da moeda {c}. Erro: {str(e)}"
+            )
+            raise e
     coin_df = pd.DataFrame(rows)
+    logger.info(
+        f"Dataframe consolidado com sucesso. Total de registros: {len(coin_df)}"
+    )
+
     bq.save_data(coin_df, dataset="bronze", table="coin_data")
--- resources.py
+++ resources.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from coingecko_sdk import Coingecko
-from dagster import ConfigurableResource, EnvVar
+from dagster import ConfigurableResource, EnvVar, get_dagster_logger
 from google.cloud import bigquery as bq
 
+logger = get_dagster_logger()
 
 class BqResource(ConfigurableResource):
     project_id: str = EnvVar("BQ_PROJECT_ID")
@@ -14,14 +15,21 @@ class BqResource(ConfigurableResource):
         table_id = f"{dataset}.{table}"
         client = self.get_client()
 
+        logger.info(f"Verificando se o dataset: {dataset} existe")
         client.create_dataset(dataset, exists_ok=True)
+        logger.info(f"Iniciando o upload de {len(df)} linhas para a tabela {table}-{table_id}")
 
-        job_config = bq.LoadJobConfig(write_disposition="WRITE_APPEND")
+        try:
+            job_config = bq.LoadJobConfig(write_disposition="WRITE_APPEND")
 
-        job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+            job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
-        job.result()
+            job.result()
 
+            logger.info(f"Upload realizado com sucesso. Dados inseridos na tabela {table_id}")
+        except Exception as e:
+            logger.error(f"Falha no upload de dados na tabela {table_id}. Erro: {str(e)}")
+            raise e
 
 class CoingeckoResource(ConfigurableResource):
     api_key: str = EnvVar("GECKO_API_KEY")
