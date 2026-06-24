24957bc4963e2703af377f921569812375972ebc
Thallys <antoniothallys2017@gmail.com>
2026-06-23 22:15:33 -0300

    feature: added multi coins extraction inside gecko_data api call

--- assets/gecko_data.py
+++ assets/gecko_data.py
@@ -9,15 +9,18 @@ from resources import BqResource, CoingeckoResource
 @asset()
 def price_df(gecko: CoingeckoResource, bq: BqResource) -> None:
     client = gecko.get_client()
-    response = client.coins.get_id("bitcoin")
 
-    json_payload = response.model_dump_json(indent=2)
+    coins = ["bitcoin", "solana", "ethereum"]
+    for c in coins:
+        response = client.coins.get_id(c)
 
-    coin_df = pd.DataFrame(
-        [
-            {
-                "ingestion_timestamp": datetime.now().isoformat(),
-                "raw_payload": json_payload,
-            }
-        ]
-    )
+        json_payload = response.model_dump_json(indent=2)
+        coin_df = pd.DataFrame(
+            [
+                {
+                    "ingestion_timestamp": datetime.now().isoformat(),
+                    "raw_payload": json_payload,
+                }
+            ]
+        )
+        bq.save_data(coin_df, dataset="bronze", table="coin_data")
